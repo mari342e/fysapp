bf932c29f0702593e2fee401e667dc3c83842f0d
Kasper Snæver <kasper@snaever.dk>
2019-04-09 14:37:14 +0200

    Looking into front page issues

--- fysapp/App.xaml.cs
+++ fysapp/App.xaml.cs
@@ -10,7 +10,7 @@ namespace fysapp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new FrontPage());
         }
 
         protected override void OnStart()
