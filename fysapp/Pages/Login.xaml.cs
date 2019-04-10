45e0807159702dcdbe996a3d857f39a325cbd7e6
Marie-Cathrine <mari342e@edu.eal.dk>
2019-04-10 10:25:44 +0200

    Creating login page

---
+++ fysapp/Pages/Login.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace fysapp.Pages
+{
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+    }
+}
