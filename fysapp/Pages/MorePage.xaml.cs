b0b0860b893419c0a4284ad5188835b8d3c5ffda
Marie-Cathrine <mari342e@edu.eal.dk>
2019-04-10 12:11:23 +0200

    Creating pages and updating navigation

---
+++ fysapp/Pages/MorePage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace fysapp.Pages
+{
+    public partial class MorePage : ContentPage
+    {
+        public MorePage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+    }
+}
