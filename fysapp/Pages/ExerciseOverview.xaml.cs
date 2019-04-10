d77906aa98e75bd436104625f24e63798a138dbe
Jesper Bille Olsen <jesp5167@edu.eal.dk>
2019-04-10 22:26:20 +0200

    Øvelses oversigt virker, med indhentning af data fra json fil

--- fysapp/Pages/ExerciseOverview.xaml.cs
+++ fysapp/Pages/ExerciseOverview.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Handler;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -14,8 +15,11 @@ namespace fysapp.Pages
 	{
 		public ExerciseOverview ()
 		{
+            var data = new Data();
 			InitializeComponent ();
             NavigationPage.SetHasNavigationBar(this, false);
+            listExerciseOverview.ItemsSource = data.GetExercises();
+
         }
 	}
 }
