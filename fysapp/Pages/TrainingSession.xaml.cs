16dcd5dbcde16b5eed986345f7781edefbfd99e6
Marie-Cathrine <mari342e@edu.eal.dk>
2019-04-11 11:49:44 +0200

    Creating page for training session

---
+++ fysapp/Pages/TrainingSession.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Handler;
+
+using Xamarin.Forms;
+
+namespace fysapp.Pages
+{
+    public partial class TrainingSession : ContentPage
+    {
+        public TrainingSession()
+        {
+            var data = new Data();
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+            listExerciseOverview.ItemsSource = data.GetExercises();
+        }
+    }
+}
