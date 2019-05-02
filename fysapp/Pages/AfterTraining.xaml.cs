e48f8738da9a7dbc51cd3aa06f536081f963cd55
Jesper Bille Olsen <jesp5167@edu.eal.dk>
2019-05-03 00:28:05 +0200

    if else på before training og after training virker + styling af aktive knapper på de pågældende sider

--- fysapp/Pages/AfterTraining.xaml.cs
+++ fysapp/Pages/AfterTraining.xaml.cs
@@ -7,6 +7,8 @@ namespace fysapp.Pages
 {
     public partial class AfterTraining : ContentPage
     {
+        bool sideEffectsAnswered = false;
+        bool tiredAnswered = false;
         public AfterTraining()
         {
             InitializeComponent();
@@ -22,6 +24,53 @@ namespace fysapp.Pages
         {
             double value = args.NewValue;
             displayNr.Text = String.Format("{0}", value);
+        }
+
+        private void HasSideEffects(object sender, EventArgs e)
+        {
+            sideEffectsAnswered = true;
+            SideEffectsFurtherQuestions.IsVisible = true;
+            YesSideEffects.BackgroundColor = Color.FromHex("#b95b5b");
+            NoSideEffects.BackgroundColor = Color.Transparent;
+            YesSideEffectsLabel.TextColor = Color.White;
+            NoSideEffectsLabel.TextColor = Color.FromHex("#707070");
+        }
+        private void NotHasSideEffects(object sender, EventArgs e)
+        {
+            sideEffectsAnswered = true;
+
+            SideEffectsFurtherQuestions.IsVisible = false;
+            NoSideEffects.BackgroundColor = Color.FromHex("#b95b5b");
+            YesSideEffects.BackgroundColor = Color.Transparent;
+            NoSideEffectsLabel.TextColor = Color.White;
+            YesSideEffectsLabel.TextColor = Color.FromHex("#707070");
+        }
+        private void Tired(object sender, EventArgs e)
+        {
+            tiredAnswered = true;
+            YesTired.BackgroundColor = Color.FromHex("#b95b5b");
+            NoTired.BackgroundColor = Color.Transparent;
+            YesTiredLabel.TextColor = Color.White;
+            NoTiredLabel.TextColor = Color.FromHex("#707070");
+        }
+        private void NotTired(object sender, EventArgs e)
+        {
+            tiredAnswered = true;            
+            NoTired.BackgroundColor = Color.FromHex("#b95b5b");
+            YesTired.BackgroundColor = Color.Transparent;
+            NoTiredLabel.TextColor = Color.White;
+            YesTiredLabel.TextColor = Color.FromHex("#707070");
+        }
+        private void SaveAfterTraining(object sender, EventArgs e)
+        {
+            if (sideEffectsAnswered && tiredAnswered)
+            {
+                GoBack(new object(), new EventArgs());
+            }
+            else
+            {
+                MissingInfoLabel.IsVisible = true;
+            }
         }
     }
 }
--- fysapp/Pages/BeforeTraining.xaml.cs
+++ fysapp/Pages/BeforeTraining.xaml.cs
@@ -7,6 +7,11 @@ namespace fysapp.Pages
 {
     public partial class BeforeTraining : ContentPage
     {
+
+        bool painkillersAnswered = false;
+        bool sideEffectsAnswered = false;
+        bool physiotherapistAnswered = false;
+
         public BeforeTraining()
         {
             InitializeComponent();
@@ -19,5 +24,70 @@ namespace fysapp.Pages
         }
 
         void OnSliderValueChanged(object sender, ValueChangedEventArgs args)         {             double value = args.NewValue;             displayNr.Text = String.Format("{0}", value);         }
+        private void HasPhysiotherapist(object sender, EventArgs e)
+        {
+            physiotherapistAnswered = true;
+            YesPhysiotherapist.BackgroundColor = Color.FromHex("#b95b5b");
+            NoPhysiotherapist.BackgroundColor = Color.Transparent;
+            YesPhysiotherapistLabel.TextColor = Color.White;
+            NoPhysiotherapistLabel.TextColor = Color.FromHex("#707070");
+        }
+        private void NotHasPhysiotherapist(object sender, EventArgs e)
+        {
+            physiotherapistAnswered = true;
+            NoPhysiotherapist.BackgroundColor = Color.FromHex("#b95b5b");
+            YesPhysiotherapist.BackgroundColor = Color.Transparent;
+            NoPhysiotherapistLabel.TextColor = Color.White;
+            YesPhysiotherapistLabel.TextColor = Color.FromHex("#707070");
+        }
+
+        private void TakenPainkillers(object sender, EventArgs e)
+        {
+            painkillersAnswered = true;
+            PainKillersFurtherQuestions.IsVisible = true;
+            YesPainkillers.BackgroundColor = Color.FromHex("#b95b5b");
+            NoPainkillers.BackgroundColor = Color.Transparent;
+            YesPainkillersLabel.TextColor = Color.White;
+            NoPainkillersLabel.TextColor = Color.FromHex("#707070");
+        }
+        private void NotTakenPainkillers(object sender, EventArgs e)
+        {
+            painkillersAnswered = true;
+            PainKillersFurtherQuestions.IsVisible = false;
+            NoPainkillers.BackgroundColor = Color.FromHex("#b95b5b");
+            YesPainkillers.BackgroundColor = Color.Transparent;
+            NoPainkillersLabel.TextColor = Color.White;
+            YesPainkillersLabel.TextColor = Color.FromHex("#707070");
+        }
+        private void HasSideEffects(object sender, EventArgs e)
+        {
+            sideEffectsAnswered = true;
+            SideEffectsFurtherQuestions.IsVisible = true;
+            YesSideEffects.BackgroundColor = Color.FromHex("#b95b5b");
+            NoSideEffects.BackgroundColor = Color.Transparent;
+            YesSideEffectsLabel.TextColor = Color.White;
+            NoSideEffectsLabel.TextColor = Color.FromHex("#707070");
+        }
+        private void NotHasSideEffects(object sender, EventArgs e)
+        {
+            sideEffectsAnswered = true;
+
+            SideEffectsFurtherQuestions.IsVisible = false;
+            NoSideEffects.BackgroundColor = Color.FromHex("#b95b5b");
+            YesSideEffects.BackgroundColor = Color.Transparent;
+            NoSideEffectsLabel.TextColor = Color.White;
+            YesSideEffectsLabel.TextColor = Color.FromHex("#707070");
+        }
+
+        private void SaveBeforeTraining(object sender, EventArgs e)
+        {
+            if (sideEffectsAnswered && painkillersAnswered && physiotherapistAnswered)
+            {
+                GoBack(new object(), new EventArgs());
+            }
+            else {
+                MissingInfoLabel.IsVisible = true;
+            }
+        }
     }
 }
