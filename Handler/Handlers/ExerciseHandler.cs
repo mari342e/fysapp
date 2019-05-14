4b2656bc04c6bd754d725b07415a761c3247dfac
Jesper Bille Olsen <jesp5167@edu.eal.dk>
2019-05-14 15:10:56 +0200

    getExercises virker igen uden umbraco api

--- Handler/Handlers/ExerciseHandler.cs
+++ Handler/Handlers/ExerciseHandler.cs
@@ -7,7 +7,6 @@ using System.IO;
 using System.Reflection;
 using System.Threading.Tasks;
 using Handler.Models;
-using System.Net.Http;
 
 namespace Handler.Handlers
 {
@@ -15,13 +14,25 @@ namespace Handler.Handlers
     {
         public async Task<List<Exercise>> GetExercisesByGroupID(int groupID)
         {
-            HttpClient client = new HttpClient();
-            var json = await client.GetStringAsync("https://fysapp-umbraco.azurewebsites.net/umbraco/api/Api/GetAllExercises");
-            List<Exercise> exerciseList = JsonConvert.DeserializeObject<List<Exercise>>(json);
-          
-            var orderedList = exerciseList.FindAll(i => i.UserGroupID == groupID);
+            var list = new List<Exercise>();
+            string jsonName = "Exercises.json";
+            var assembly = typeof(ExerciseHandler).GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream($"{assembly.GetName().Name}.{jsonName}");
 
-            return orderedList;
+            using (var reader = new StreamReader(stream))
+            {
+                var jsonString = reader.ReadToEnd();
+
+                list = JsonConvert.DeserializeObject<List<Exercise>>(jsonString);
+            }
+            var orderedList = list.FindAll(i => i.UserGroupID == groupID);
+
+            return list;
+            //HttpClient client = new HttpClient();
+            //var json = await client.GetStringAsync("https://fysapp-umbraco.azurewebsites.net/umbraco/api/Api/GetAllExercises");
+            //List<Exercise> exerciseList = JsonConvert.DeserializeObject<List<Exercise>>(json);
+
+            //var orderedList = exerciseList.FindAll(i => i.UserGroupID == groupID);
         }
 
     }
