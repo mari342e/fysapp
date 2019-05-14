611dae0327fb7265e5fe04859a22adc45036be77
Jesper Bille Olsen <jesp5167@edu.eal.dk>
2019-05-14 14:50:51 +0200

    Henter exercises via api (mangler at blive testet)

--- Handler/Handlers/ExerciseHandler.cs
+++ Handler/Handlers/ExerciseHandler.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Reflection;
 using System.Threading.Tasks;
 using Handler.Models;
+using System.Net.Http;
 
 namespace Handler.Handlers
 {
@@ -14,20 +15,13 @@ namespace Handler.Handlers
     {
         public async Task<List<Exercise>> GetExercisesByGroupID(int groupID)
         {
-            var list = new List<Exercise>();
-            string jsonName = "Exercises.json";
-            var assembly = typeof(ExerciseHandler).GetTypeInfo().Assembly;
-            Stream stream = assembly.GetManifestResourceStream($"{assembly.GetName().Name}.{jsonName}");
+            HttpClient client = new HttpClient();
+            var json = await client.GetStringAsync("https://fysapp-umbraco.azurewebsites.net/umbraco/api/Api/GetAllExercises");
+            List<Exercise> exerciseList = JsonConvert.DeserializeObject<List<Exercise>>(json);
+          
+            var orderedList = exerciseList.FindAll(i => i.UserGroupID == groupID);
 
-            using (var reader = new StreamReader(stream))
-            {
-                var jsonString = reader.ReadToEnd();
-
-                list = JsonConvert.DeserializeObject<List<Exercise>>(jsonString);
-            }
-            var orderedList = list.FindAll(i => i.UserGroupID == groupID);
-
-            return list;
+            return orderedList;
         }
 
     }
